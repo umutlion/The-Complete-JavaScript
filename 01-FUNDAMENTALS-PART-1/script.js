c595efa400b2f69794e0793c84c5aa572e3127be
umutlion <umutcayli@outlook.com>
2022-01-19 00:24:57 +0300

    first commit.

---
+++ 01-FUNDAMENTALS-PART-1/script.js
@@ -0,0 +1,26 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Umut");
+console.log(23);
+
+let firstName = "Umut";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// Variabla name conventions
+let umut_cayli = "UC";
+let $function = 27;
+
+let person = "Umut";
+let PI = 3.1415;
+
+let myFirstJob = "Developer";
+let myCurrentJob = "Programmer";
+
+true;
+console.log(true);
+let javascriptIsFun = true;
