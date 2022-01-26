3cd94fe1222a37dbb0014944481d154c5244441c
umutlion <umutcayli@outlook.com>
2022-01-27 00:57:46 +0300

    added strict mode and functions (declarations vs expressions).

---
+++ 02-FUNDAMENTALS-PART-2/script.js
@@ -0,0 +1,51 @@
+/* "use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = 'Audio';
+// const private = 534;
+
+*/
+/* 
+function logger() {
+  console.log("My name is Umut");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+ */
+/* 
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1998);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1998);
+
+console.log(age1, age2) */
+
