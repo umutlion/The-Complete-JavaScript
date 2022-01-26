f777ae66ef6494813ec33007fc1e675aae1d60b3
umutlion <umutcayli@outlook.com>
2022-01-27 01:13:08 +0300

    added arrow func.

--- 02-FUNDAMENTALS-PART-2/script.js
+++ 02-FUNDAMENTALS-PART-2/script.js
@@ -49,3 +49,24 @@ const age2 = calcAge2(1998);
 
 console.log(age1, age2) */
 
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+// Arrow Function
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1998, "Umut ÇAYLI"));
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
